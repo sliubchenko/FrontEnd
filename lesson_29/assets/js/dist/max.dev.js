2fb03530b183ea18ecb344bca51d7dea4acc10ae
sliubchenko <sliubchenko@gmail.com>
2024-11-22 19:03:47 +0100

    l29 upd

--- lesson_29/assets/js/dist/max.dev.js
+++ lesson_29/assets/js/dist/max.dev.js
@@ -1,13 +1,13 @@
 "use strict";
 
 var radius = prompt("Set the radius");
-var area = 3.14 * radius * radius;
+var area = parseFloat((Math.PI * radius * radius).toFixed(3));
 alert("The area is ".concat(area));
 var distance = prompt("Set the distance in km");
 var trip_time = prompt("Set desired trip time in hours");
-var expected_speed = distance / trip_time;
+var expected_speed = parseFloat((distance / trip_time).toFixed(3));
 alert("Expected speed is ".concat(expected_speed, " km/h"));
 var usd = prompt("Set USD amount");
 var rate = 0.96;
-var euro = usd * rate;
+var euro = parseFloat((usd * rate).toFixed(2));
 alert("".concat(usd, " USD is ").concat(euro, " EURO"));
--- lesson_29/assets/js/max.js
+++ lesson_29/assets/js/max.js
@@ -1,13 +1,13 @@
 const radius = prompt("Set the radius");
-const area = 3.14 * radius * radius;
+const area = parseFloat((Math.PI * radius * radius).toFixed(3));
 alert(`The area is ${area}`);
 
 const distance = prompt("Set the distance in km");
 const trip_time = prompt("Set desired trip time in hours");
-const expected_speed = distance / trip_time;
+const expected_speed = parseFloat((distance / trip_time).toFixed(3));
 alert(`Expected speed is ${expected_speed} km/h`);
 
 const usd = prompt("Set USD amount");
 const rate = 0.96;
-const euro = usd * rate;
+const euro = parseFloat((usd * rate).toFixed(2));
 alert(`${usd} USD is ${euro} EURO`);
