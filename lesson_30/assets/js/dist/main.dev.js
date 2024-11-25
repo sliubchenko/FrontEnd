8afd7ceab10f526dd5cd66faea837e6a5a78b028
sliubchenko <sliubchenko@gmail.com>
2024-11-25 14:59:49 +0100

    l30 fix

--- lesson_30/assets/js/dist/main.dev.js
+++ lesson_30/assets/js/dist/main.dev.js
@@ -11,23 +11,46 @@ var f = parseInt(d) + e;
 alert("2. The sum of \"1\" and 2 is ".concat(f));
 var usb = parseInt(prompt('Enter your usb-drive size in GB'));
 var file = 820;
-var files_qty = Math.floor(usb * 1024 / file);
-alert("3. You can save ".concat(files_qty, " files on your drive")); //Norm
+
+if (!isNaN(usb)) {
+  var files_qty = Math.floor(usb * 1024 / file);
+  alert("3. You can save ".concat(files_qty, " files on your drive"));
+} else {
+  alert('Wrong format!');
+} //Norm
+
 
 var budget = parseFloat(prompt('Enter your budget'));
 var choco_price = parseFloat(prompt('Enter choco price'));
-var choco_amount = Math.floor(budget / choco_price);
-var rest = (budget - choco_price * choco_amount).toFixed(2);
-alert("4. You can buy ".concat(choco_amount, " chocolate bars and get the rest ").concat(rest));
+
+if (!isNaN(budget) && !isNaN(choco_price)) {
+  var choco_amount = Math.floor(budget / choco_price);
+  var rest = (budget - choco_price * choco_amount).toFixed(2);
+  alert("4. You can buy ".concat(choco_amount, " chocolate bars and get the rest ").concat(rest));
+} else {
+  alert('Wrong format!');
+}
+
 var number = parseInt(prompt('Enter your 3-digit number'));
-var reverse = String(number % 10);
-number = parseInt(number / 10);
-reverse += String(number % 10);
-number = parseInt(number / 10);
-reverse += String(number);
-alert("5. Reverse is ".concat(reverse)); //Max
+
+if (!isNaN(number) && number < 1000 && number > 99) {
+  var reverse = String(number % 10);
+  number = parseInt(number / 10);
+  reverse += String(number % 10);
+  number = parseInt(number / 10);
+  reverse += String(number);
+  alert("5. Reverse is ".concat(reverse));
+} else {
+  alert('Wrong format!');
+} //Max
+
 
 var deposit = parseFloat(prompt('What is deposit amount?'));
-var profit = (deposit * 0.05 / 12 * 2).toFixed(2); //     / 12 * 2    for clarily
 
-alert("6. Your profit will be ".concat(profit));
+if (!isNaN(deposit)) {
+  var profit = (deposit * 0.05 / 12 * 2).toFixed(2); //     / 12 * 2    for clarily
+
+  alert("6. Your profit will be ".concat(profit));
+} else {
+  alert('Wrong format!');
+}
--- lesson_30/assets/js/main.js
+++ lesson_30/assets/js/main.js
@@ -11,29 +11,49 @@ alert(`2. The sum of "1" and 2 is ${f}`);
 
 const usb = parseInt(prompt('Enter your usb-drive size in GB'));
 const file = 820;
-const files_qty = Math.floor(usb * 1024 / file);
-alert(`3. You can save ${files_qty} files on your drive`);
+if (!isNaN(usb)){
+    const files_qty = Math.floor(usb * 1024 / file);
+    alert(`3. You can save ${files_qty} files on your drive`);
+}
+else {
+    alert('Wrong format!')
+}
 
 
 
 //Norm
 const budget = parseFloat(prompt('Enter your budget'));
 const choco_price = parseFloat(prompt('Enter choco price'));
-const choco_amount = Math.floor(budget / choco_price);
-const rest = (budget - choco_price * choco_amount).toFixed(2);
-alert(`4. You can buy ${choco_amount} chocolate bars and get the rest ${rest}`);
+if (!isNaN(budget) && !isNaN(choco_price)){
+    const choco_amount = Math.floor(budget / choco_price);
+    const rest = (budget - choco_price * choco_amount).toFixed(2);
+    alert(`4. You can buy ${choco_amount} chocolate bars and get the rest ${rest}`);
+}
+else {
+    alert('Wrong format!')
+}
 
 let number = parseInt(prompt('Enter your 3-digit number'));
-let reverse = String(number % 10);
-number = parseInt(number / 10);
-reverse += String(number % 10);
-number = parseInt(number / 10);
-reverse += String(number);
-alert(`5. Reverse is ${reverse}`);
+if (!isNaN(number) && number < 1000 && number >99){
+    let reverse = String(number % 10);
+    number = parseInt(number / 10);
+    reverse += String(number % 10);
+    number = parseInt(number / 10);
+    reverse += String(number);
+    alert(`5. Reverse is ${reverse}`);
+}
+else {
+    alert('Wrong format!')
+}
 
 
 
 //Max
 const deposit = parseFloat(prompt('What is deposit amount?'));
-const profit = (deposit * 0.05 / 12 * 2).toFixed(2); //     / 12 * 2    for clarily
-alert(`6. Your profit will be ${profit}`);
+if (!isNaN(deposit)){
+    const profit = (deposit * 0.05 / 12 * 2).toFixed(2); //     / 12 * 2    for clarily
+    alert(`6. Your profit will be ${profit}`);
+}
+else {
+    alert('Wrong format!')
+}
